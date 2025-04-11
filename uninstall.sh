29890fa5e32645a78eef6e5dbc6846de06093fc8
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2025-04-11 10:38:36 +1000

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Uninstalling Wi-Fi OSINT Tool..."
+rm -rf ~/.config/wifi-osint ~/.local/share/wifi-osint
+rm ~/bin/wifi-osint
+echo "Uninstallation complete."
